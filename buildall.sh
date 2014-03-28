fc8f0822e99d7dc9c0a0ac8bfe9b468e7afb242f
Flo Lauber <fl-dev@qatfy.at>
2014-03-27 23:46:25 -0600

    windows 386 binary

---
+++ buildall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /home/flo/git/golang-crosscompile/crosscompile.bash;
+go-linux-amd64 build -o bin/linux_amd64/hync main.go;
+go-windows-386 build -o bin/windows_386/hync.exe main.go;
+go-windows-amd64 build -o bin/windows_amd64/hync.exe main.go;
